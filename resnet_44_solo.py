4da0e447e0a6ac94f24d09084bf965a45a9b6166
Felix O'Mahony <felixomahony@gmail.com>
2023-12-28 11:27:41 +0000

    update-model

--- resnet_44_solo.py
+++ resnet_44_solo.py
@@ -35,14 +35,14 @@ transform_train = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     # transforms.ToTensor(),
     HueSeparation(1),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0, 0, 0), (0.2023, 0.1994, 0.2010)),
     TensorReshape(),
 ])
 
 transform_test = transforms.Compose([
     # transforms.ToTensor(),
     HueSeparation(1),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0, 0, 0), (0.2023, 0.1994, 0.2010)),
     TensorReshape(),
 ])
 
