fa83c3b30741f7be9aea2e8ee8f49cb61f169426
Felix O'Mahony <felixomahony@gmail.com>
2024-02-01 20:53:19 +0100

    shapes3d

---
+++ experiment_5_2_1_ceconv.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+from torch.optim.lr_scheduler import MultiStepLR
+
+from vision_transforms import HueSeparation, TensorReshape
+from traintest import run
+from traintest import dataloaders
+
+n_groups = 1
+n_groups_luminance=1
+
+print("Experiment 5_2_1")
+print("Shapes3D")
+print("lr=0.01")
+
+trainloader, testloader = dataloaders.shapes3d(n_groups)
+
+if __name__=="__main__":
+    run.run(
+        trainloader=trainloader, 
+        testloader=testloader, 
+        nt="resnet44_ceconv", 
+        n_groups=n_groups,
+        num_classes=4,
+        luminance=False,
+        n_groups_luminance=n_groups_luminance,
+        n_epochs=None,
+        n_iters=100_000,
+        use_scheduler=False,
+        lr=0.01,)
